2589fcf87486c7ac507971323fb7033f3cb463ca
Crazy4bricks <59067881+Crazy4bricks@users.noreply.github.com>
2025-03-18 06:44:33 -0700

    Refactoring

---
+++ pyRPG/utils/color.py
@@ -0,0 +1,42 @@
+true_black = (0x0, 0x0, 0x0)
+white = (0xFF, 0xFF, 0xFF)
+gray = (177, 201, 195)
+bright_red = (215, 66, 0)
+dark_red = (166, 74, 46)
+bright_orange = (233, 159, 16)
+dark_orange = (241, 95, 34)
+yellow = (207, 192, 65)
+brown = (152, 135, 95)
+bright_green = (0, 196, 32)
+dark_green = (0, 148, 3)
+black = (21, 83, 82)
+dark_black = (15, 59, 58)
+bright_cyan = (119, 191, 207)
+dark_cyan = (64, 164, 185)
+bright_blue = (0, 150, 255)
+dark_blue = (0, 72, 189)
+magenta = (218, 91, 214)
+dark_magenta = (177, 84, 207)
+
+player_atk = white
+enemy_atk = bright_red
+needs_target = bright_cyan
+status_effect_applied = bright_green
+descend = white
+
+player_die = bright_red
+enemy_die = bright_orange
+
+invalid = yellow
+impossible = gray
+error = bright_red
+
+welcome_text = yellow
+health_recovered = bright_green
+
+bar_text = white
+bar_filled = dark_green
+bar_empty = dark_red
+
+menu_title = yellow
+menu_text = white
